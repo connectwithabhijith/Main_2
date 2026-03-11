f738a0167e7bcb2a865237833cc3efb1a2e6dd16
Abhijith <connectwithabhijithrs@gmail.com>
2026-03-11 22:21:59 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ app.use(cors({
   origin: [
     'http://localhost:8080',
     'http://localhost:5173',
+    'http://localhost:5174',
     'http://localhost:3000',
     process.env.FRONTEND_URL
   ].filter(Boolean),
